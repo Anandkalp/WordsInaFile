ca9c9aa8331da0a17291e7085c79dd8de4836d0e
Anandkalp <anandk.neo@gmail.com>
2022-06-07 08:31:10 +0530

    added

---
+++ WordsInaFile2.java
@@ -0,0 +1,16 @@
+public class WordsInaFile2
+{
+public static void main(String args[])
+	{
+		String s1="Welcomr to New Java Learner";
+		int count=1;
+		for(int i=0; i<s1.length()-1; i++)
+		{
+			if((s1.charAt(i)==' ') && (s1.charAt(i+1)!=' '))
+			{
+				count++;
+			}
+		}
+		System.out.print("No of Words : "+count);
+	}
+}
